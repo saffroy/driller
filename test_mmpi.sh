26cdfc80726b9d39ddf10370f8a0a9cd740b82f2
Jean-Marc Saffroy <saffroy@gmail.com>
2007-09-24 01:52:48 +0200

    Don't use taskset by default.

--- test_mmpi.sh
+++ test_mmpi.sh
@@ -7,7 +7,7 @@ iter=${2:-1000000}
 
 for i in $(seq 0 $((nprocs-1)) ); do
 	#strace -fo strace-$i ./test_mmpi $jobid $nprocs $i $iter &
-	#./test_mmpi $jobid $nprocs $i $iter &
-	taskset -c $i ./test_mmpi $jobid $nprocs $i $iter &
+	./test_mmpi $jobid $nprocs $i $iter &
+	#taskset -c $i ./test_mmpi $jobid $nprocs $i $iter &
 done
 wait
